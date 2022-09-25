e06996f729badc3c447c2e77fa770a32d0931599
Zubair <zubairkamboh9010@gmail.com>
2022-09-25 11:22:07 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.use('/app', express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'zubair',
+    email: 'zubairkamboh9010@gmail.com',
+  })
+})
+
+const port = process.env.PORT || 8000
+app.listen(port)
